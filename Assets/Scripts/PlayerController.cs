0366e48f9a78283f421d0b0d96a5db2b181394e3
alelievr <alelievr@student.42.fr>
2017-11-17 15:47:22 +0100

    added scene manager and flappy bird controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum PlayerControl
+{
+	Flappy,
+
+}
+
+public class PlayerController : MonoBehaviour
+{
+	[Header("Global settings")]
+	public PlayerControl	control;
+	public float			defaultWindForce = 1f;
+	public float			maxXVelocity = 3f;
+
+	[Space]
+	[Header("Falppy control settings")]
+	public float			flappyUpForce = 50;
+
+	Rigidbody2D				rbody;
+	public bool				dead { get; private set; }
+
+	Dictionary< PlayerControl, Action >	controlActions = new Dictionary< PlayerControl, Action >();
+	Dictionary< PlayerControl, Action >	fixedControlActions = new Dictionary< PlayerControl, Action >();
+
+	void Start ()
+	{
+		if (dead)
+			return ;
+		
+		rbody = GetComponent< Rigidbody2D >();
+		
+		controlActions[PlayerControl.Flappy] = UpdateFlappy;
+
+		fixedControlActions[PlayerControl.Flappy] = FixedUpdateFlappy;
+	}
+	
+	void Update ()
+	{
+		if (dead)
+			return ;
+		controlActions[control]();
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		Debug.Log("tag: " + other.collider.tag);
+		switch (other.collider.tag)
+		{
+			case "Water":
+				Death();
+				break ;
+			case "Obstacle":
+				Death();
+				break ;
+		}
+	}
+
+	void Death()
+	{
+		Debug.Log("you're dead");
+		dead = true;
+	}
+
+	void FixedUpdate()
+	{
+		if (fixedControlActions.ContainsKey(control))
+			fixedControlActions[control]();
+		rbody.velocity = new Vector2(Mathf.Clamp(rbody.velocity.x, -maxXVelocity, maxXVelocity), rbody.velocity.y);
+	}
+
+	void UpdateFlappy()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rbody.velocity = new Vector2(rbody.velocity.x, 0);
+			rbody.AddForce(Vector2.up * flappyUpForce, ForceMode2D.Impulse);
+		}
+	}
+
+	void FixedUpdateFlappy()
+	{
+		rbody.AddForce(Vector2.right * defaultWindForce);
+	}
+}
---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+
+	public float			fadeTime = 1f;
+	public AnimationCurve	fadeCurve;
+	public AudioSource		fadeAudioSource;
+
+	public string titleScreenSceneName = "TitleScreen";
+	public string historySceneName = "History";
+	public string explorationSceneName = "Exploration";
+	public string creditsSceneame = "Credits";
+	public string craftSceneName = "CreaftScreen";
+	public string travelSceneName = "Travel";
+
+	public static SceneSwitcher instance;
+
+	void Awake()
+	{
+		instance = this;
+		DontDestroyOnLoad(this);
+	}
+
+	public void ShowTitleScreen()
+	{
+		StartCoroutine(FadeScene(titleScreenSceneName));
+	}
+
+	public void ShowHistory()
+	{
+		StartCoroutine(FadeScene(historySceneName));
+	}
+
+	public void ShowExploration()
+	{
+		StartCoroutine(FadeScene(explorationSceneName));
+	}
+
+	public void ShowTravel()
+	{
+		StartCoroutine(FadeScene(travelSceneName));
+	}
+
+	public void ShowCraft()
+	{
+		StartCoroutine(FadeScene(craftSceneName));
+	}
+
+	public void ShowCredits()
+	{
+		StartCoroutine(FadeScene(creditsSceneame));
+	}
+
+	IEnumerator FadeOut(Image panel)
+	{
+		float	startTime = Time.time;
+		float	alpha;
+		
+		do
+		{
+			alpha = 1 - ((Time.time - startTime) / fadeTime);
+			alpha = fadeCurve.Evaluate(alpha);
+			fadeAudioSource.volume = 1 - alpha;
+			panel.color = new Color(0, 0, 0, alpha);
+			yield return null;
+		} while (alpha > 0f);
+	}
+
+	IEnumerator FadeIn(Image panel)
+	{
+		float	startTime = Time.time;
+		float	alpha;
+		
+		do
+		{
+			alpha = ((Time.time - startTime) / fadeTime);
+			alpha = fadeCurve.Evaluate(alpha);
+			fadeAudioSource.volume = 1 - alpha;
+			panel.color = new Color(0, 0, 0, alpha);
+			yield return null;
+		} while (alpha < 1f);
+	}
+
+	IEnumerator FadeScene(string sceneName)
+	{
+		Image panel = GameObject.Find("fullScreenPanel").GetComponent< Image >();
+
+		yield return FadeIn(panel);
+		SceneManager.LoadScene(sceneName);
+
+		panel = GameObject.Find("fullScreenPanel").GetComponent< Image >();
+		yield return FadeOut(panel);
+	}
+
+}
